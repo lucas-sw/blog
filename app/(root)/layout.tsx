15a4a3e83a8c80e79750fa83d0156ee3e42bf1c0
lucas-sw <lucas_sw512@163.com>
2025-01-06 01:00:19 +0800

    添加 abount 页面跳转

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,8 @@
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "首页",
+};
 
 export default function Layout({ children }: React.PropsWithChildren) {
   return (
--- constants/path.ts
+++ constants/path.ts
@@ -1,15 +1,24 @@
 import { NICK_NAME } from "./info";
 
 
-export const PATHS = {
-    SITE_HOME: "/",
-    SITE_ABOUT: "/about",
-    SITE_BLOG: "/blog",
+// 定义一个字符串枚举
+export enum PATHS {
+    SITE_HOME = "/",
+    SITE_ABOUT = "/about",
+    SITE_BLOG = "/blog",
 }
 
-export const PATHS_DESCRIPTION: Record<keyof typeof PATHS, string> = {
-    SITE_HOME: "首页",
-    SITE_ABOUT: "关于",
-    SITE_BLOG: "博客",
-}
+// 使用枚举的键作为类型定义路径描述
+export const PATHS_STRING: Record<PATHS, string> = {
+    [PATHS.SITE_HOME]: "首页",
+    [PATHS.SITE_ABOUT]: "关于",
+    [PATHS.SITE_BLOG]: "博客",
+};
+
+export const PATH_DESCRIPTION_MAP: Record<PATHS, string> = {
+    /** ************* SITE ****************** */
+    [PATHS.SITE_HOME]: "首页",
+    [PATHS.SITE_BLOG]: "这里记录了我的想法、文章，希望和大家一起交流～",
+    [PATHS.SITE_ABOUT]: `叮～ 你有一份关于${NICK_NAME}的简介，请查收～`,
+  };
 
